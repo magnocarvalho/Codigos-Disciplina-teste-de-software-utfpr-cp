c1fee3e5f544416628bd978f6ce920c1ac2e7403
Aluno <Aluno@I205-557860>
2018-04-02 18:46:18 -0300

    test de software atividade

---
+++ AtividadesTestSoftware/src/br/edu/utfpr/magneira/Extremos.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.magneira;
+
+/**
+ *
+ * @author Aluno
+ */
+class Extremos {
+
+    int menor, maior, indiceMaior, indiceMenor;
+
+    public Extremos(int menor, int indiceMenor, int maior, int indiceMaior) {
+        this.menor = menor;
+        this.maior = maior;
+        this.indiceMaior = indiceMaior;
+        this.indiceMenor = indiceMenor;
+    }
+
+    public int getMenor() {
+        return menor;
+    }
+
+    public int getMaior() {
+        return maior;
+    }
+
+    public int getIndiceMaior() {
+        return indiceMaior;
+    }
+
+    public int getIndiceMenor() {
+        return indiceMenor;
+    }
+    
+   
+    
+}
